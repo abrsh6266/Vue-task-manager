d76ff2bfac3167f76c9fc1e6773e1e666c7154a9
abrsh6266 <abrsh6266@gmail.com>
2023-10-17 03:32:23 +0900

    updating each files

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button @btn-click  ="$emit('toggle-add-task')" 
+        :text="showAddTask? 'Close' : 'Add Task'" 
+        :color="showAddTask? 'red' : 'green'" 
+        v-show="homePage"
+        />
+    </header>
+</template>
+<script>
+import Button from "./Button"
+export default{
+    name: 'Header' , 
+    props:{
+        title: String,
+        showAddTask:Boolean
+    },
+    components: {
+    Button,
+  },
+  computed:{
+    homePage(){
+        if(this.$route.path==='/'){
+            return true
+        }else return false
+    }
+  }
+}
+</script>
+<Style scoped>
+    header {
+        display: flex;
+        justify-conent: space-between;
+        align-items: center;
+        margin-bottom: 20px
+    }
+</Style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,78 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+      <AddTask v-show="showAddTask" @add-task="AddTask" />
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Tasks from '../components/Tasks'
+import AddTask from '../components/AddTask'
+export default{
+    name: 'HomeView',
+    components:{
+        Tasks,
+        AddTask,
+    },
+    data(){
+        return {
+            tasks:[],
+        }
+    },
+    props:{
+        showAddTask: Boolean
+    },
+    async created(){
+    this.tasks = await this.fetchTasks()
+    },
+    methods:{
+    async deleteTask(id){
+      if(confirm('are you sure?')){
+        const res = await fetch(`api/tasks/${id}`,{
+          method:'DELETE',})
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+          res.status===200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : 
+          alert('Error Deleting')
+      }
+    },
+    async toggleReminder(id){
+      const taskToToggle = await this.fetchTask(id)
+      const   updTask = {...taskToToggle, reminder:!taskToToggle.reminder}
+
+      const res = await fetch(`api/tasks/${id}`,{
+        method: 'PUT',
+        headers:{
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updTask)
+      })
+      this.tasks = this.tasks.map((task) => task.id === id ? {
+        ...task, reminder: updTask.reminder} :task
+      )
+    },
+    async AddTask(task){
+      const res = await fetch('api/tasks',{
+        method: 'POST',
+        headers:{
+          'Content-type':'application/json',
+         
+        },
+        body: JSON.stringify(task),
+      })
+
+      const data = await res.json()
+      
+      this.tasks = [...this.tasks, data]
+    },
+    async fetchTasks(){
+      const res = await fetch('api/tasks')
+      const data = await res.json()
+      
+      return data
+    },
+    async fetchTask(id){
+      const res = await fetch(`api/tasks/${id}`)
+      const data = await res.json()
+      
+      return data
+    }
+  },
 }
 </script>
